953ee4c1775e911598fb2d3daa96257542d18841
mark-eskander <119497072+mark-eskander@users.noreply.github.com>
2025-01-19 20:36:06 +0200

    Update tailwind.config.js
    add some animation keywords

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'vibrate': {
+          '0%, 100%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(-10px)' },
+          '50%': { transform: 'translateX(10px)' },
+          '75%': { transform: 'translateX(-10px)' },
+        },
+        'fade-in': {
+          from: {
+            opacity: '0',
+            transform: 'translateX(-30px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        'fade-in-form': {
+          from: {
+            opacity: '0',
+            transform: 'translateX(30px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+      },
+      animation:{
+        'vibrate': 'vibrate 0.9s ease-in-out 1',
+        'fade-in': 'fade-in 2s',
+        'fade-in-form' : 'fade-in-form 2s'
+      }
+    },
   },
   plugins: [],
 }
