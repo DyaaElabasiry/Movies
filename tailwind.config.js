b7cc17131d4b04515e418ac03d1a371c674980cd
farah ahmed <farahahmedxx0@gmail.com>
2025-01-22 12:51:39 +0200

    restructuring done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './client/public/*.html',
+    './client/public/src/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
