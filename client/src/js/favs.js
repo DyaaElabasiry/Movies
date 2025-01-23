3f7d606a087a408002f34d742c0e8e6b8a90963b
ahmed-elhoshy <elhoushyahmedd@gmail.com>
2025-01-23 20:51:11 +0200

    added navigate to details from favs and edited styles and final touches

--- client/src/js/favs.js
+++ client/src/js/favs.js
@@ -20,7 +20,6 @@ function fetchUserFavorites() {
     });
 }
 
-// Fetch movie details from TMDb API
 function fetchMovieDetails(movieId) {
   const apiKey = "d10b21c479b4bc082f3fab5d7cc62326";
   const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
@@ -35,7 +34,6 @@ function fetchMovieDetails(movieId) {
     });
 }
 
-// Render a movie card
 function showMovieCard(movie) {
   const movieCard = `
     <div id="movie-${
@@ -43,7 +41,7 @@ function showMovieCard(movie) {
     }" class=" movie-card mx-3 flex-shrink-0 relative hover:brightness-110  animate-[fadeIn] transition duration-500 ease-in-out transform hover:scale-105 cursor-pointer">
       <i id="deleteIcon-${
         movie.id
-      }" class="material-icons bg-gradient-to-r from-white to-blue-500 text-transparent bg-clip-text absolute top-7 right-3 lg:top-8 lg:right-10 cursor-pointer">delete</i>
+      }" class="material-icons bg-gradient-to-r from-white to-blue-500 text-transparent bg-clip-text absolute top-7 right-3 lg:top-8 lg:right-10 cursor-pointer hover:text-blue-500  transition duration-300">delete</i>
             <img
         id="poster_image-${
           movie.id
@@ -51,10 +49,11 @@ function showMovieCard(movie) {
         src="https://image.tmdb.org/t/p/original${movie.poster_path}"
         alt="${movie.title}"
       />
-      <p class="movie-title ml-3 md:ml-3 lg:ml-11 text-base sm:text-lg text-gray-900 font-semibold">
-        ${movie.title}
+<p
+  class="movie-title md:ml-3 lg:ml-10 text-base sm:text-lg text-white font-semibold"
+>        ${movie.title}
       </p>
-      <p class="movie-date ml-3 md:ml-3 lg:ml-11 text-sm text-gray-500">
+      <p class="movie-date md:ml-3 lg:ml-10 text-sm text-gray-400">
         ${movie.release_date.slice(0, 4)}
       </p>
     </div>
@@ -62,7 +61,6 @@ function showMovieCard(movie) {
 
   favoritesContainer.insertAdjacentHTML("beforeend", movieCard);
 
-  // Add event listener to the delete icon
   document
     .getElementById(`deleteIcon-${movie.id}`)
     .addEventListener("click", () => {
@@ -70,30 +68,26 @@ function showMovieCard(movie) {
       removeFavoriteMovie(movie.id);
     });
 
-  // document
-  //   .getElementById(`poster_image-${movie.id}`)
-  //   .addEventListener("click", () => {
-  //     window.location.href = `details.html?id=${movie.id}`;
-  //   });
+  document
+    .getElementById(`poster_image-${movie.id}`)
+    .addEventListener("click", function () {
+      window.location.href = `details.html?type=${movie.type || "movie"}&id=${
+        movie.id
+      }`;
+    });
 }
-
-// Remove a movie from the user's favorites
 function removeFavoriteMovie(movieId) {
   console.log(`Removing movie ID: ${movieId}`);
 
-  // Fetch the current user data
   fetch(`http://localhost:3000/users/${userId}`)
     .then((response) => response.json())
     .then((user) => {
       console.log("Current user data:", user);
 
-      // Remove the movie ID from the user's favourites array
       const updatedFavorites = user.favourites.filter(
         (id) => id !== movieId.toString()
-      ); // Ensure movieId is a string
+      );
       console.log("Updated favorites:", updatedFavorites);
-
-      // Update the user data on the server
       fetch(`http://localhost:3000/users/${userId}`, {
         method: "PATCH",
         headers: {
@@ -106,13 +100,11 @@ function removeFavoriteMovie(movieId) {
         .then(() => {
           console.log("Movie removed from favorites");
 
-          // Remove the movie card from the DOM
           const movieElement = document.getElementById(`movie-${movieId}`);
           if (movieElement) {
             movieElement.remove();
           }
 
-          // If no favorites are left, display a message
           if (updatedFavorites.length === 0) {
             favoritesContainer.innerHTML = `<p class="text-gray-800 text-center">No favorite movies found.</p>`;
           }
@@ -126,7 +118,6 @@ function removeFavoriteMovie(movieId) {
     });
 }
 
-// Initialize the script
 window.onload = function () {
   fetchUserFavorites();
 };
--- client/src/js/home.js
+++ client/src/js/home.js
@@ -1,34 +1,39 @@
 // Navbar
-const mobileMenuButton = document.getElementById('mobileMenuButton');
-const mobileMenu = document.getElementById('mobileMenu');
+const mobileMenuButton = document.getElementById("mobileMenuButton");
+const mobileMenu = document.getElementById("mobileMenu");
 
-mobileMenuButton.addEventListener('click', () => {
-  mobileMenu.classList.toggle('hidden');
+mobileMenuButton.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
 });
 
 // Generic Scroll Function
-function setupScroll(containerId, leftButtonId, rightButtonId, scrollAmount = 300) {
+function setupScroll(
+  containerId,
+  leftButtonId,
+  rightButtonId,
+  scrollAmount = 300
+) {
   const container = document.getElementById(containerId);
   const leftButton = document.getElementById(leftButtonId);
   const rightButton = document.getElementById(rightButtonId);
 
-  leftButton.addEventListener('click', () => {
-    container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+  leftButton.addEventListener("click", () => {
+    container.scrollBy({ left: -scrollAmount, behavior: "smooth" });
   });
 
-  rightButton.addEventListener('click', () => {
-    container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+  rightButton.addEventListener("click", () => {
+    container.scrollBy({ left: scrollAmount, behavior: "smooth" });
   });
 }
 
 // Setup Scrolling for Sections
-setupScroll('scrollContainer', 'scrollLeft', 'scrollRight');
-setupScroll('scrollContainerSeries', 'scrollLeftSeries', 'scrollRightSeries');
-setupScroll('scrollContainerActors', 'scrollLeftActors', 'scrollRightActors');
+setupScroll("scrollContainer", "scrollLeft", "scrollRight");
+setupScroll("scrollContainerSeries", "scrollLeftSeries", "scrollRightSeries");
+setupScroll("scrollContainerActors", "scrollLeftActors", "scrollRightActors");
 
 // API Configuration
-const Api_Key = 'bf6de15b7b7a3f0005e93bbde1715684';
-const base_Url = 'https://api.themoviedb.org/3';
+const Api_Key = "bf6de15b7b7a3f0005e93bbde1715684";
+const base_Url = "https://api.themoviedb.org/3";
 
 // API Endpoints
 const api_Url = `${base_Url}/trending/movie/day?language=en-US&api_key=${Api_Key}`;
@@ -36,56 +41,57 @@ const search_Movie_Url = `${base_Url}/search/movie?api_key=${Api_Key}`;
 const search_Series_Url = `${base_Url}/search/tv?api_key=${Api_Key}`;
 const series_Url = `${base_Url}/tv/popular?api_key=${Api_Key}&language=en-US`;
 const actors_Url = `${base_Url}/person/popular?api_key=${Api_Key}&language=en-US`;
-const imges_Url = 'https://image.tmdb.org/t/p/w500';
+const imges_Url = "https://image.tmdb.org/t/p/w500";
 
 // Fallback Image URL
-const fallbackImage = 'https://assets.vogue.in/photos/5f16b3bc9ffca08d1848369b/2:3/w_2560%2Cc_limit/must-watch%2520action%2520movies.jpg';
+const fallbackImage =
+  "https://assets.vogue.in/photos/5f16b3bc9ffca08d1848369b/2:3/w_2560%2Cc_limit/must-watch%2520action%2520movies.jpg";
 
 // DOM Elements
-const main = document.querySelector('.main');
-const search = document.getElementById('searchInput');
-const form = document.getElementById('searchForm');
-const tagsEl = document.getElementById('tags');
+const main = document.querySelector(".main");
+const search = document.getElementById("searchInput");
+const form = document.getElementById("searchForm");
+const tagsEl = document.getElementById("tags");
 let selectedGenre = [];
 
 // Genres
 const genres = [
-  { id: 28, name: 'Action' },
-  { id: 12, name: 'Adventure' },
-  { id: 16, name: 'Animation' },
-  { id: 35, name: 'Comedy' },
-  { id: 80, name: 'Crime' },
-  { id: 99, name: 'Documentary' },
-  { id: 18, name: 'Drama' },
-  { id: 10751, name: 'Family' },
-  { id: 14, name: 'Fantasy' },
-  { id: 36, name: 'History' },
-  { id: 27, name: 'Horror' },
-  { id: 10402, name: 'Music' },
-  { id: 9648, name: 'Mystery' },
-  { id: 10749, name: 'Romance' },
-  { id: 878, name: 'Science Fiction' },
-  { id: 10770, name: 'TV Movie' },
-  { id: 53, name: 'Thriller' },
-  { id: 10752, name: 'War' },
-  { id: 37, name: 'Western' }
+  { id: 28, name: "Action" },
+  { id: 12, name: "Adventure" },
+  { id: 16, name: "Animation" },
+  { id: 35, name: "Comedy" },
+  { id: 80, name: "Crime" },
+  { id: 99, name: "Documentary" },
+  { id: 18, name: "Drama" },
+  { id: 10751, name: "Family" },
+  { id: 14, name: "Fantasy" },
+  { id: 36, name: "History" },
+  { id: 27, name: "Horror" },
+  { id: 10402, name: "Music" },
+  { id: 9648, name: "Mystery" },
+  { id: 10749, name: "Romance" },
+  { id: 878, name: "Science Fiction" },
+  { id: 10770, name: "TV Movie" },
+  { id: 53, name: "Thriller" },
+  { id: 10752, name: "War" },
+  { id: 37, name: "Western" },
 ];
 
 // Set Genres
 function setGenre() {
-  const genreDropdown = document.getElementById('genreDropdown');
-  genreDropdown.innerHTML = '';
+  const genreDropdown = document.getElementById("genreDropdown");
+  genreDropdown.innerHTML = "";
 
   genres.forEach((genre) => {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.innerHTML = `
       <a href="#" class="block text-xlg px-4 py-2 text-base text-gray-900 hover:bg-gray-50 hover:text-blue-600 transition duration-300" data-genre-id="${genre.id}">
         ${genre.name}
       </a>
     `;
-    li.querySelector('a').addEventListener('click', (e) => {
+    li.querySelector("a").addEventListener("click", (e) => {
       e.preventDefault();
-      const genreId = e.target.getAttribute('data-genre-id');
+      const genreId = e.target.getAttribute("data-genre-id");
       fetchMoviesAndSeriesByGenre([genreId]); // Fetch movies and series by genre
     });
     genreDropdown.appendChild(li);
@@ -94,7 +100,7 @@ function setGenre() {
 
 // Fetch Movies and Series by Genre
 function fetchMoviesAndSeriesByGenre(selectedGenre) {
-  const genreQuery = encodeURI(selectedGenre.join(','));
+  const genreQuery = encodeURI(selectedGenre.join(","));
 
   // Fetch movies by genre
   const moviesUrl = `${base_Url}/discover/movie?api_key=${Api_Key}&with_genres=${genreQuery}`;
@@ -102,21 +108,28 @@ function fetchMoviesAndSeriesByGenre(selectedGenre) {
 
   Promise.all([
     fetch(moviesUrl).then((res) => res.json()),
-    fetch(seriesUrl).then((res) => res.json())
-  ])
-    .then(([moviesData, seriesData]) => {
-      // Combine results
-      const combinedResults = [...moviesData.results, ...seriesData.results];
-      showCombinedResults(combinedResults); // Display the results
-    })
+    fetch(seriesUrl).then((res) => res.json()),
+  ]).then(([moviesData, seriesData]) => {
+    // Combine results
+    const combinedResults = [...moviesData.results, ...seriesData.results];
+    showCombinedResults(combinedResults); // Display the results
+  });
 }
 
 // function to create a media card for movies and series
 function createMediaCard(media, type) {
-  const { title, name, poster_path, vote_average, release_date, first_air_date, id } = media;
+  const {
+    title,
+    name,
+    poster_path,
+    vote_average,
+    release_date,
+    first_air_date,
+    id,
+  } = media;
 
-  const mediaTitle = type === 'movie' ? title : name;
-  const mediaDate = type === 'movie' ? release_date : first_air_date;
+  const mediaTitle = type === "movie" ? title : name;
+  const mediaDate = type === "movie" ? release_date : first_air_date;
   const imageUrl = poster_path ? `${imges_Url}${poster_path}` : fallbackImage;
 
   // Return the card HTML
@@ -127,7 +140,9 @@ function createMediaCard(media, type) {
         <img class="w-full h-48 sm:h-56 lg:h-64 object-cover cursor-pointer" src="${imageUrl}" alt="${mediaTitle}" onerror="this.src='${fallbackImage}'" onclick="redirectToDetails('${type}', ${id})">
         <div class="absolute bottom-0 left-0 right-0 bg-black bg-opacity-75 p-2">
           <div class="flex items-center justify-between">
-            <span class="text-sm font-bold text-yellow-400">${(vote_average * 10).toFixed(0)}%</span>
+            <span class="text-sm font-bold text-yellow-400">${(
+              vote_average * 10
+            ).toFixed(0)}%</span>
             <!-- Trailer button -->
             <button class="bg-blue-500 text-white px-2 py-1 rounded-full text-sm hover:bg-red-600 transition-colors duration-300" onclick="event.stopPropagation(); fetchTrailer('${type}', ${id})">
               Trailer
@@ -145,18 +160,18 @@ function createMediaCard(media, type) {
 
 // Create a movie card
 function createMovieCard(movie) {
-  return createMediaCard(movie, 'movie');
+  return createMediaCard(movie, "movie");
 }
 
 // Create a series card
 function createSeriesCard(series) {
-  return createMediaCard(series, 'tv');
+  return createMediaCard(series, "tv");
 }
 
 // redirect to details page
 function redirectToDetails(type, id) {
   const detailsUrl = `details.html?type=${type}&id=${id}`; // a8dt el-url 3ashan i will use to redirect for deails page
-  window.location.href = detailsUrl;   // hna ana har7 3la el-details page
+  window.location.href = detailsUrl; // hna ana har7 3la el-details page
 }
 
 // Function to fetch trailer
@@ -165,74 +180,86 @@ function fetchTrailer(type, id) {
     .then((res) => res.json())
     .then((videoData) => {
       if (videoData.results && videoData.results.length > 0) {
-        const trailer = videoData.results.find((video) => video.type === 'Trailer');
+        const trailer = videoData.results.find(
+          (video) => video.type === "Trailer"
+        );
         if (trailer) {
           const videoUrl = `https://www.youtube.com/embed/${trailer.key}`;
           openVideoOverlay(videoUrl);
         } else {
-          alert('No trailer available for this item.');
+          alert("No trailer available for this item.");
         }
       } else {
-        alert('No trailer available for this item.');
+        alert("No trailer available for this item.");
       }
-    })
+    });
 }
 
 // Video Overlay Functions
 function openVideoOverlay(videoUrl) {
-  const videoOverlay = document.getElementById('videoOverlay');
-  const videoContainer = document.getElementById('videoContainer');
+  const videoOverlay = document.getElementById("videoOverlay");
+  const videoContainer = document.getElementById("videoContainer");
 
   // Clear previous video
-  videoContainer.innerHTML = '';
+  videoContainer.innerHTML = "";
 
   // Create the iframe for the video
-  const iframe = document.createElement('iframe');
+  const iframe = document.createElement("iframe");
   iframe.src = videoUrl;
-  iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture';
+  iframe.allow =
+    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
   iframe.allowFullscreen = true;
-  iframe.classList.add('w-full', 'h-96', 'sm:h-128', 'rounded-lg', 'shadow-lg');
+  iframe.classList.add("w-full", "h-96", "sm:h-128", "rounded-lg", "shadow-lg");
 
   // Append the iframe to the video container
   videoContainer.appendChild(iframe);
 
   // Show the overlay
-  videoOverlay.classList.remove('hidden');
+  videoOverlay.classList.remove("hidden");
 }
 
 function closeVideoOverlay() {
-  const videoOverlay = document.getElementById('videoOverlay');
-  const videoContainer = document.getElementById('videoContainer');
+  const videoOverlay = document.getElementById("videoOverlay");
+  const videoContainer = document.getElementById("videoContainer");
 
   // Clear the video
-  videoContainer.innerHTML = '';
+  videoContainer.innerHTML = "";
 
   // Hide the overlay
-  videoOverlay.classList.add('hidden');
+  videoOverlay.classList.add("hidden");
 }
 
 // Add event listener to the close button
-document.getElementById('closeVideoButton').addEventListener('click', closeVideoOverlay);
+document
+  .getElementById("closeVideoButton")
+  .addEventListener("click", closeVideoOverlay);
 
 // Search Functionality
 let searchTimeout;
-search.addEventListener('input', () => {
+search.addEventListener("input", () => {
   clearTimeout(searchTimeout);
   searchTimeout = setTimeout(() => {
     const searchAction = search.value.trim();
     if (searchAction) {
       // Fetch both movies and series
       Promise.all([
-        fetch(`${search_Movie_Url}&query=${encodeURIComponent(searchAction)}`).then((res) => res.json()),
-        fetch(`${search_Series_Url}&query=${encodeURIComponent(searchAction)}`).then((res) => res.json())
+        fetch(
+          `${search_Movie_Url}&query=${encodeURIComponent(searchAction)}`
+        ).then((res) => res.json()),
+        fetch(
+          `${search_Series_Url}&query=${encodeURIComponent(searchAction)}`
+        ).then((res) => res.json()),
       ])
         .then(([moviesData, seriesData]) => {
           // Combine results
-          const combinedResults = [...moviesData.results, ...seriesData.results];
+          const combinedResults = [
+            ...moviesData.results,
+            ...seriesData.results,
+          ];
           showCombinedResults(combinedResults); // Display the results
         })
         .catch((error) => {
-          console.error('Error fetching search results:', error);
+          console.error("Error fetching search results:", error);
         });
     } else {
       // If search is empty, show trending movies and popular series
@@ -248,7 +275,7 @@ function getMovies(url) {
     .then((res) => res.json())
     .then((data) => {
       showMovies(data.results);
-    })
+    });
 }
 
 // Fetch Series
@@ -257,7 +284,7 @@ function getSeries(url) {
     .then((res) => res.json())
     .then((data) => {
       showSeries(data.results);
-    })
+    });
 }
 
 // Fetch Actors
@@ -266,16 +293,16 @@ function getActors(url) {
     .then((res) => res.json())
     .then((data) => {
       showActors(data.results);
-    })
+    });
 }
 
 // Show Movies
 function showMovies(data) {
-  const moviesContainer = document.getElementById('scrollContainer');
-  moviesContainer.innerHTML = '';
+  const moviesContainer = document.getElementById("scrollContainer");
+  moviesContainer.innerHTML = "";
 
   data.forEach((movie) => {
-    const movieElement = document.createElement('div');
+    const movieElement = document.createElement("div");
     movieElement.innerHTML = createMovieCard(movie);
     moviesContainer.appendChild(movieElement);
   });
@@ -283,11 +310,11 @@ function showMovies(data) {
 
 // Show Series
 function showSeries(data) {
-  const seriesContainer = document.getElementById('scrollContainerSeries');
-  seriesContainer.innerHTML = '';
+  const seriesContainer = document.getElementById("scrollContainerSeries");
+  seriesContainer.innerHTML = "";
 
   data.forEach((series) => {
-    const seriesElement = document.createElement('div');
+    const seriesElement = document.createElement("div");
     seriesElement.innerHTML = createSeriesCard(series);
     seriesContainer.appendChild(seriesElement);
   });
@@ -295,16 +322,16 @@ function showSeries(data) {
 
 // Show Combined Results
 function showCombinedResults(data) {
-  const moviesContainer = document.getElementById('scrollContainer');
-  const seriesContainer = document.getElementById('scrollContainerSeries');
+  const moviesContainer = document.getElementById("scrollContainer");
+  const seriesContainer = document.getElementById("scrollContainerSeries");
 
   // Clear both containers
-  moviesContainer.innerHTML = '';
-  seriesContainer.innerHTML = '';
+  moviesContainer.innerHTML = "";
+  seriesContainer.innerHTML = "";
 
   data.forEach((item) => {
     const isMovie = item.title;
-    const element = document.createElement('div');
+    const element = document.createElement("div");
 
     if (isMovie) {
       element.innerHTML = createMovieCard(item);
@@ -318,15 +345,23 @@ function showCombinedResults(data) {
 
 // Show Actors
 function showActors(data) {
-  const actorsContainer = document.getElementById('scrollContainerActors');
-  actorsContainer.innerHTML = '';
+  const actorsContainer = document.getElementById("scrollContainerActors");
+  actorsContainer.innerHTML = "";
 
   data.forEach((actor) => {
     const { name, profile_path, known_for_department } = actor;
-    const actorElement = document.createElement('div');
-    actorElement.classList.add('actor', 'mx-3', 'flex-shrink-0', 'relative', 'hover:brightness-110');
+    const actorElement = document.createElement("div");
+    actorElement.classList.add(
+      "actor",
+      "mx-3",
+      "flex-shrink-0",
+      "relative",
+      "hover:brightness-110"
+    );
 
-    const imageUrl = profile_path ? `${imges_Url}${profile_path}` : fallbackImage;
+    const imageUrl = profile_path
+      ? `${imges_Url}${profile_path}`
+      : fallbackImage;
 
     actorElement.innerHTML = `
       <div class="relative">
